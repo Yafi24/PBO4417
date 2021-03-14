432a896a5a78b6bad173876ca855aaaecf803f58
Yafi24 <80208815+Yafi24@users.noreply.github.com>
2021-03-14 16:57:09 +0700

    Add files via upload

--- Pertemuan 2/Praktik/Manusia.java
+++ Pertemuan 2/Praktik/Manusia.java
@@ -1,1 +1,21 @@
-
+public class Manusia {
+    String nama = "noname";
+    boolean punyaTV = false;
+    
+    public Manusia(){}
+    public Manusia(String nama){
+        this.nama = nama;
+    }
+    public String namaSaya() {
+        return nama;
+    }
+    public void beliTV(){
+        punyaTV = true;
+    }
+    public void jualTV() {
+        punyaTV = false;
+    }
+    public boolean cekTV(boolean punyaTV){
+        return punyaTV;
+    }
+}
---
+++ Pertemuan 2/Praktik/TestTV.java
@@ -0,0 +1,9 @@
+public class TestTV {
+    public static void main(String[] args) {
+        Manusia man1 = new Manusia("Agus Lukito");
+        System.out.println(man1.namaSaya());
+
+        TV tvku = new TV();
+        man1.beliTV();
+    }
+}
