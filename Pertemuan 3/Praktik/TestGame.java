b371a127bb6ed1a28a55aa7bf91447e688e06ccc
Yafi24 <80208815+Yafi24@users.noreply.github.com>
2021-03-23 19:49:59 +0700

    Add files via upload

---
+++ Pertemuan 3/Praktik/TestGame.java
@@ -0,0 +1,9 @@
+public class TestGame {
+    public static void main(String[] args){
+    GamePlayer one = new GamePlayer();
+    GameEnemy  two = new GameEnemy();
+    one.run();
+    one.Run(30);
+    two.run();
+    }
+}
