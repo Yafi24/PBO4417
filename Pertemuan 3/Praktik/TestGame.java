6a8b6d1edb0ebdf54591d569ecab448e95cdb0b9
Yafi24 <80208815+Yafi24@users.noreply.github.com>
2021-03-23 19:47:28 +0700

    Delete TestGame.java

--- Pertemuan 3/Praktik/TestGame.java
+++
@@ -1,9 +0,0 @@
-public class TestGame {
-    public static void main(String[] args){
-    GamePlayer one = new GamePlayer();
-    GameEnemy  two = new GameEnemy();
-    one.run();
-    one.Run(30);
-    two.run();
-    }
-}
